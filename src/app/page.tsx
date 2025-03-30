0941253fc172ab7c7450e719e278bceb9d53f847
Hoang Vinh <mvinh418@gmail.com>
2025-03-30 23:25:50 +0700

    add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,18 @@ import Occasions from "./components/Occasions";
 import TopSellers from "./components/TopSellers";
 import SaleOff from "./components/SaleOff";
 import BlogSection from "./components/BlogSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Occasions/>
-      <TopSellers/>
-      <SaleOff/>
-      <BlogSection/>
+      <Occasions />
+      <TopSellers />
+      <SaleOff />
+      <BlogSection />
+      <Footer />
     </div>
   );
 }
