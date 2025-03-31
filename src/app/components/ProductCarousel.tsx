9af0cb341e6e0c29413a69400b5e690bebb254ee
UPIN-0583 <quochuystg2004@gmail.com>
2025-03-31 15:02:41 +0700

    Sửa lại TopSeller

--- src/app/components/ProductCarousel.tsx
+++ src/app/components/ProductCarousel.tsx
@@ -22,30 +22,51 @@ interface ProductCarouselProps {
 
 const ProductCarousel: React.FC<ProductCarouselProps> = ({ products }) => {
   return (
-    <div className="mt-8 relative mx-48">
-      <div className="mx-10">
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={30}
-          slidesPerView={4}
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          }}
-          loop={true}
-        >
-          {products.map((item, index) => (
-            <SwiperSlide key={index}>
+    <div className="mt-8 relative mx-4 md:mx-12 lg:mx-32">
+    <div className="mx-auto max-w-[95%] sm:max-w-[90%]">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={20}
+        slidesPerView={1}
+        centeredSlides={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            centeredSlides: false,
+            spaceBetween: 30
+          },
+          1000: {
+            slidesPerView: 3,
+            centeredSlides: false,
+            spaceBetween: 30
+          },
+          1385: {
+            slidesPerView: 4,
+            centeredSlides: false,
+            spaceBetween: 30
+          },
+        }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        loop={true}
+        className="!px-4"
+      >
+        {products.map((item, index) => (
+          <SwiperSlide key={index} className="flex justify-center items-center py-4">
+            <div className="w-full flex justify-center">
               <ProductCard {...item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      {/* Custom Navigation Buttons */}
-      <div className="swiper-button-next !text-purple-800"></div>
-      <div className="swiper-button-prev !text-purple-800"></div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
+
+    {/* Custom Navigation Buttons */}
+    <div className="swiper-button-next !text-purple-800 !-right-2"></div>
+    <div className="swiper-button-prev !text-purple-800 !-left-2"></div>
+  </div>
   );
 };
 
--- src/app/components/TopSellers.tsx
+++ src/app/components/TopSellers.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-// import ProductCarousel from "./ProductCarousel";
-import ProductCard from "./ProductCard";
-import { Navigation } from "swiper/modules";
+//import { Swiper, SwiperSlide } from "swiper/react";
+import ProductCarousel from "./ProductCarousel";
+//import ProductCard from "./ProductCard";
+//import { Navigation } from "swiper/modules";
 
 
 const products = [
@@ -29,52 +29,8 @@ export default function TopSellers() {
           View All Products
         </button>
       </div>
+      <ProductCarousel products={products} />
 
-      <div className="mt-8 relative mx-4 md:mx-12 lg:mx-32">
-        <div className="mx-auto max-w-[95%] sm:max-w-[90%]">
-          <Swiper
-            modules={[Navigation]}
-            spaceBetween={20}
-            slidesPerView={1}
-            centeredSlides={true}
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-                centeredSlides: false,
-                spaceBetween: 30
-              },
-              1000: {
-                slidesPerView: 3,
-                centeredSlides: false,
-                spaceBetween: 30
-              },
-              1385: {
-                slidesPerView: 4,
-                centeredSlides: false,
-                spaceBetween: 30
-              },
-            }}
-            navigation={{
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            }}
-            loop={true}
-            className="!px-4"
-          >
-            {products.map((item, index) => (
-              <SwiperSlide key={index} className="flex justify-center items-center py-4">
-                <div className="w-full flex justify-center">
-                  <ProductCard {...item} />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-
-        {/* Custom Navigation Buttons */}
-        <div className="swiper-button-next !text-purple-800 !-right-2"></div>
-        <div className="swiper-button-prev !text-purple-800 !-left-2"></div>
-      </div>
     </section>
   );
 }
