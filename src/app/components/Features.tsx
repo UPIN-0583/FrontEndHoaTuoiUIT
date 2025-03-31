7a64d7cc87ee2c6759e254d9382bf82747d74894
UPIN-0583 <quochuystg2004@gmail.com>
2025-03-31 23:23:52 +0700

    Sửa resposive cho trang checkout, shoppingcart, productdetails

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -4,31 +4,31 @@ import { faShippingFast, faWallet, faHeadset } from "@fortawesome/free-solid-svg
 export default function Features() {
   return (
     <div className="py-10 bg-white">
-        <div className="flex justify-between mx-52 ">
+        <div className="flex flex-col md:flex-row justify-center gap-10">
             {/* Free Shipping */}
-            <div className="flex items-center gap-3 ">
+            <div className="flex items-center gap-3 justify-center">
                 <FontAwesomeIcon icon={faShippingFast} className="text-purple-500 text-3xl" />
                 <div>
-                <h4 className="font-semibold text-lg text-black">Free Shipping</h4>
-                <p className="text-gray-500 text-sm">Free shipping for order above $50</p>
+                    <h4 className="font-semibold text-lg text-black">Free Shipping</h4>
+                    <p className="text-gray-500 text-sm">Free shipping for order above $50</p>
                 </div>
             </div>
 
             {/* Flexible Payment */}
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 justify-center">
                 <FontAwesomeIcon icon={faWallet} className="text-purple-500 text-3xl" />
                 <div>
-                <h4 className="font-semibold text-lg text-black">Flexible Payment</h4>
-                <p className="text-gray-500 text-sm">Multiple secure payment options</p>
+                    <h4 className="font-semibold text-lg text-black">Flexible Payment</h4>
+                    <p className="text-gray-500 text-sm">Multiple secure payment options</p>
                 </div>
             </div>
 
             {/* 24x7 Support */}
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 justify-center">
                 <FontAwesomeIcon icon={faHeadset} className="text-purple-500 text-3xl" />
                 <div>
-                <h4 className="font-semibold text-lg text-black">24×7 Support</h4>
-                <p className="text-gray-500 text-sm">We support online all days.</p>
+                    <h4 className="font-semibold text-lg text-black">24×7 Support</h4>
+                    <p className="text-gray-500 text-sm">We support online all days.</p>
                 </div>
             </div>
         </div>
