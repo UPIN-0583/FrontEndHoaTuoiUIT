3ac77771bcf5a6cdef7337700a1103dc52a8ae97
UPIN-0583 <145818482+UPIN-0583@users.noreply.github.com>
2025-05-12 14:28:39 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://hoatuoiuit.id.com', // ✅ Thay bằng domain thật của bạn
+  siteUrl: 'https://hoatuoiuit.id.vn', // ✅ Thay bằng domain thật của bạn
   generateRobotsTxt: true,           // Tạo robots.txt luôn
   changefreq: 'daily',               // Cập nhật sitemap mỗi ngày
   priority: 0.7,
