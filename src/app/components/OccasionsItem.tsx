85ca059461b1de8fa5833d929b2604962ead76ed
Hoang Vinh <mvinh418@gmail.com>
2025-05-15 18:14:08 +0700

    add login/signup + test fetch occasions

--- src/app/components/OccasionsItem.tsx
+++ src/app/components/OccasionsItem.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 
-interface OssasionsItemProps{
-    title: string;
-    products: number;
-    img:string;
+interface OssasionsItemProps {
+    name: string;
+    description: string;
+    imageUrl: string;
 }
-export default function OccasionsItem({ title, products, img }: OssasionsItemProps){
+export default function OccasionsItem({ name, imageUrl }: OssasionsItemProps) {
     return (
         <div className="flex flex-col items-center w-40">
-                <div className="w-24 h-24 bg-gray-100 rounded-full flex items-center justify-center">
-                    <Image src={img} alt={title} width={60} height={60} />
-                </div>
-                <h4 className="mt-2 font-semibold text-lg text-black">{title}</h4>
-                <p className="text-gray-500 text-sm">{products} Products</p>
+            <div className="w-24 h-24 bg-gray-100 rounded-full flex items-center justify-center">
+                <Image src={imageUrl} alt={name} width={60} height={60} />
+            </div>
+            <h4 className="mt-2 font-semibold text-lg text-black">{name}</h4>
         </div>
     )
 };
